302ec654944155ed26a6fa2e529513e67f535330
bassi <rohitbassi.007.rb@gmail.com>
2018-05-17 18:05:25 +0530

    http

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var http = require('https');
+var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
+  console.log("running")
   res.write('Hello World!');
   res.end();
 }).listen(3000);
