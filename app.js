87956da7259db3a9fff95eed8011c21a19b601bc
bassi <rohitbassi.007.rb@gmail.com>
2018-05-17 18:13:10 +0530

    typo

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Hello World!');
-  req.end();
+  res.end();
 }).listen(3000);
