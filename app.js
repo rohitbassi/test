57efc330f931927be7d1888ea99acc5bf4670055
bassi <rohitbassi.007.rb@gmail.com>
2018-05-17 14:31:25 +0530

    changes in http call

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var http = require('http');
-http.createServer(function (req, res) {
+var https = require('https');
+https.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Hello World!');
   res.end();
