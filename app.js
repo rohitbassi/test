d7d933676468e8594ffeb7e47ec77472b8b0d6b7
bassi <rohitbassi.007.rb@gmail.com>
2018-05-18 10:53:38 +0530

    server changes

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
-  res.end();
-}).listen(3000);
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write('Hello World!');
+//   res.end();
+// }).listen(3000);
+
+var http = require('https');
+
+var server = http.createServer(function (request, response) {
+  var reqBody = '';
+  request.on('data', function (chunk) {
+        reqBody += chunk;
+  });
+
+  request.on('end', function (chunk) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("<h1>Hello world!</h1>");
+    console.log(reqBody);
+  });
+});
+
+server.listen(3000);
